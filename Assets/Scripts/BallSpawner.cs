3db2a5f99fca78128fc12c962fa13d2fa036ab59
falcon <imadake398yen@gmail.com>
2018-11-04 20:43:56 +0900

    first commit

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+	[SerializeField] private GameObject ballPrefab;
+	[SerializeField] private Transform arrow;
+	private Transform tapPoint;
+	private float direction;
+	const float power = 10f;
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			if (tapPoint == null) {
+				tapPoint = new GameObject().transform;
+			}
+		}
+		if (Input.GetMouseButton(0)) {
+			var aTapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			tapPoint.transform.position = new Vector3(aTapPoint.x, aTapPoint.y, 0);
+			arrow.transform.LookAt(tapPoint);
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			StartCoroutine(Shot());
+		}
+	}
+
+	private IEnumerator Shot () {
+		for (var i=0; i<10; i++) {
+			var ball = Instantiate(ballPrefab);
+			ball.transform.position = transform.position;
+			var r = ball.GetComponent<Rigidbody2D>();
+			r.velocity = arrow.transform.forward * power;
+			yield return new WaitForSeconds(0.1f);
+		}
+		yield return null;
+	}
+
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Obstacle : MonoBehaviour {
+
+	[SerializeField] Text label;
+	private int life;
+
+	private void Start () {
+		life = Random.Range(1, 10);
+		label.text = life.ToString();
+	}
+
+	private void OnCollisionEnter2D (Collision2D other) {
+		var ball = other.gameObject.GetComponent<Ball>();
+		if (ball != null) {
+			life -= 1;
+			label.text = life.ToString();
+			if (life == 0) Destroy(gameObject);
+		}
+	}
+
+}
